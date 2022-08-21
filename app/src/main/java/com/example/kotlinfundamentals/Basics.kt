a3f47498ee6ee9fe1d67fbcb04e8a3e3998fa2e4
Yiro Yi <yiroyi@live.com>
2022-08-21 13:07:10 +0900

    Setup project for Kotlin Basics

---
+++ app/src/main/java/com/example/kotlinfundamentals/Basics.kt
@@ -0,0 +1,56 @@
+package com.example.kotlinfundamentals
+
+fun main(){
+    // inmutable variable
+//    val myName = "Yiro Yi" //this has type innference the first value assigned will be the data type
+//
+//    // Integers
+//    var myAge = 31
+//
+//    // Floating points
+//    val myFloat: Float = 12.37F
+//    val myDouble: Double = 3.1415783278372
+//
+//    //Booleans
+//    var isSunny: Boolean = true
+//    isSunny = false
+//
+//    //Characters
+//    val letterChar = 'A'
+//    val digitChar = '1'
+//
+//    //Strings
+//    val myString = "Hello World"
+//    var firstCharacter = myString[0]
+//
+//    var masterClassString: String = "Android Masterclass"
+//    var theFloat: Float = 13.37F
+//    var theDouble: Double = 3.141726782
+//    var theInteger: Int = 25
+//    var theLong: Long =34233232
+//    var theBoolean: Boolean = true
+//    var theCharacter: Char = 'a'
+//
+//    // String Interpolation
+//    // Concatenation
+//    print("Hello " + myName)
+//    //Interpolation is done with $ sign
+//    print("Hello $myName")
+//    print("Hello " + myName)
+//    //Interpolation of functions
+//    print("Hello $myName and the length is ${masterClassString.length}")
+
+//    // If statements
+//    var heightPerson1 = 180
+//    var heightPerson2 = 180
+//
+//    if (heightPerson1 > heightPerson2) {
+//        println("Use raw force")
+//    } else if (heightPerson1 == heightPerson2) {
+//        println("Use draw")
+//    } else {
+//        println("Use technique")
+//    }
+
+    
+}
