2e0af5bfb2392e5f48855755e220e23c7312147d
Yiro Yi <yiroyi@live.com>
2022-08-24 22:37:52 +0900

    Exception handling

---
+++ app/src/main/java/com/example/kotlinfundamentals/Exception.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinfundamentals
+
+// Exception is a runtime problem errors and leads to program termination
+
+// Throwable Class
+// keywords: try, catch, finally: used to execute important code statement, throw: it is used to
+// throw an exeption explicitly
+
+// Unchecked exception is thrown due to mistakes in code and it is considered run time exception
+// for example ArithmeticException ArrayIndexOutOfBoundExceptions, SecurityExceptions
+
+// Checked Exceptions
+
+//try {
+//  code..
+//} catch {
+//
+//}
